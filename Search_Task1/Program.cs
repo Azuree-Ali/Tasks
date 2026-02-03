c5b2fe0cbd36fc9ecc135217401685cd6de98c03
Azuree <Azuree@DESKTOP-1QT4HAC>
2026-02-03 21:21:37 +0200

    The third point of the search Task 1

---
+++ Search_Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Search_Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 15;
+            double y = 4;
+            Console.WriteLine($"Equation: 15 / 4 = {x / y:F2}");
+        }
+    }
+}
