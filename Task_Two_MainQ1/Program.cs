54b85a65cc24ef5a198bd790641ae22a99ae25c4
Azuree <Azuree@DESKTOP-1QT4HAC>
2026-02-09 22:17:24 +0200

    Add question 1 in main task 2

---
+++ Task_Two_MainQ1/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Task_Two_MainQ1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of days : ");
+            int days = Convert.ToInt32(Console.ReadLine());
+            if (days < 0)
+            {
+                Console.WriteLine("Invalid input. Days cannot be negative.");
+            }
+            else if (days < 10)
+            {
+                Console.WriteLine("Not Eligible");
+            }
+            else if (days <= 19)
+            {
+                Console.WriteLine("Eligible");
+            }
+            else if (days >= 20)
+            {
+                Console.WriteLine("Excellent Attendance");
+            }
+            else
+            {
+                Console.WriteLine("Invalid value");
+            }
+        }
+    }
+}
